828e9347e20c0494f1e0d0804e865fb14784063f
Jayden Ly <jjly1502@gmail.com>
2022-09-08 02:52:52 +1000

    implemented room link sharing

--- client/src/components/PlayerBoard.js
+++ client/src/components/PlayerBoard.js
@@ -1,87 +1,111 @@
-import React, { useState, useEffect, useContext } from 'react';
-import styled from 'styled-components';
+import React, { useState, useEffect, useContext } from "react";
+import styled from "styled-components";
 import { useLocation, Navigate } from "react-router-dom";
-import { SocketContext } from '../context/socket';
-import ParticlesBackground from './Particles';
+import { SocketContext } from "../context/socket";
+import ParticlesBackground from "./Particles";
+import ClipboardCopy from "./ClipboardCopy";
 
-const Circle = styled.div`
+
+const Wrapper = styled.div`
+    position: relative;
 `
+const Circle = styled.div``;
 
 const Board = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    min-height: 100vh;
-`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+	min-height: 100vh;
+`;
 
 const Player = styled.div`
-    width: 100px;
-    height: 100px;
-    border-radius: 20px;
-    background-color: blue;
-    position: absolute;
-    transform: translate3d(-50%, -50%, 0);
-    
-    display: flex;
-    justify-content: center;
-    align-items: center;
+	width: 100px;
+	height: 100px;
+	border-radius: 20px;
+	background-color: blue;
+	position: absolute;
+	transform: translate3d(-50%, -50%, 0);
 
-    & > p {
-        font-family: Roboto;
-        font-size: 20px;
-        color: white;
-    }
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	& > p {
+		font-family: Roboto;
+		font-size: 20px;
+		color: white;
+	}
+`;
+
+const ClipboardCopyS = styled(ClipboardCopy)`
+    position: absolute;
+    top: 10%;
+    right: 10%;
 `
 
 const PlayerBoard = () => {
-    const socket = useContext(SocketContext);
-    const location = useLocation();
+	const socket = useContext(SocketContext);
+	const location = useLocation();
 
-    var initState = location.state || null;
-    
-    const [players, setPlayers] = useState(initState);
-    const numPlayers = players ? players.length : -1;
-    
-    useEffect(() => {
-        socket.on('joinSuccess', (data) => setPlayers(data));
-        return () => {
-		    socket.off('joinSuccess', (data) => setPlayers(data));
+	var initState = location.state || null;
+
+	const [players, setPlayers] = useState(initState);
+	const numPlayers = players ? players.length : -1;
+
+	useEffect(() => {
+		socket.on("joinSuccess", (data) => setPlayers(data));
+		return () => {
+			socket.off("joinSuccess", (data) => setPlayers(data));
 		};
-    }, [socket]) 
-    
-    let size = 400;
-    let r = size/2 + 80;
-  
-    function degToRad(deg) {
-        return deg * Math.PI / 180;
-    }
+	}, [socket]);
 
-    let theta = 2*Math.PI/numPlayers;
+	let size = 400;
+	let r = size / 2 + 80;
 
-    return (
-        players ? 
-        <Board>
-            <ParticlesBackground></ParticlesBackground>
-            <Circle style={{backgroundColor: "red", 
-                width: size,
-                height: size,
-                borderRadius: size/2}}>
-            </Circle>
+	function degToRad(deg) {
+		return (deg * Math.PI) / 180;
+	}
 
-            {players.map((val, idx) => {
-                return(
-                    <Player style={{
-                        top: `calc(${r*Math.cos(2*Math.PI/numPlayers*idx)}px + 50%)`,
-                        left: `calc(${r*Math.sin(2*Math.PI/numPlayers*idx)}px + 50%)`
-                    }}>
-                        <p>{val.username}</p>
-                    </Player>
-                )
-            })}
-        </Board> :
-        <Navigate to="/" state={{room: window.location.pathname.slice(7)}}/>
-    )
-}
+	let theta = (2 * Math.PI) / numPlayers;
+
+	return players ? (
+		<Wrapper>
+            <ClipboardCopyS copyText={window.location.pathname}/>
+			<Board>
+				{/* <ParticlesBackground></ParticlesBackground> */}
+				<Circle
+					style={{
+						border: "1px solid red",
+						width: size,
+						height: size,
+						borderRadius: size / 2,
+					}}
+				></Circle>
+
+				{players.map((val, idx) => {
+					return (
+						<Player
+							style={{
+								top: `calc(${
+									r *
+									Math.cos(((2 * Math.PI) / numPlayers) * idx)
+								}px + 50%)`,
+								left: `calc(${
+									r *
+									Math.sin(((2 * Math.PI) / numPlayers) * idx)
+								}px + 50%)`,
+							}}
+						>
+							<p>{val.username}</p>
+						</Player>
+					);
+				})}
+			</Board>
+		</Wrapper>
+	) : (
+		<Navigate to="/" state={{ room: window.location.pathname.slice(7) }} />
+	);
+};
 
 export default PlayerBoard;
--- server/players.js
+++ server/players.js
@@ -5,6 +5,9 @@ const joinRoom = (rooms, room, username, socketId) => {
     currentPlayers = rooms.get(room)
     if (!currentPlayers.some(player => player.username === username)) {
         currentPlayers.push({ username, socketId })
+        return 0;
+    } else {
+        return -1;
     }
 }   
 
